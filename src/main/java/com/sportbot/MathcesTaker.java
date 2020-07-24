c7f0c4ca1f8e2b295123db39610c9ccc632a6587
Khagan <azizovkhgn@gmail.com>
2020-07-24 12:52:58 +0400

    Initial commit

---
+++ src/main/java/com/sportbot/MathcesTaker.java
@@ -0,0 +1,87 @@
+package com.sportbot;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverInfo;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class MathcesTaker extends Thread {
+    public static StringBuilder matches;
+    static Thread t=new Thread();
+    String binpath=System.getenv("GOOGLE_CHROME_BIN");
+    String chromedriverpath=System.getenv("CHROMEDRIVER_PATH");
+    static Object lock=new Object();
+    @Override
+    public void run() {
+        synchronized (MathcesTaker.lock) {
+            matches = new StringBuilder();
+            System.out.println(binpath);
+            System.out.println(chromedriverpath);
+            System.setProperty("webdriver.chrome.driver", chromedriverpath);
+            ChromeOptions options = new ChromeOptions();
+            options.setHeadless(true);
+            options.setBinary(binpath);
+            WebDriver driver = new ChromeDriver(options);
+            driver.get("https://www.livescore.com/soccer/2020-07-24/");
+            WebDriverWait  wait=new WebDriverWait(driver, Duration.ofSeconds(30));
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@data-type='container' and @style='display: block;']")));
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            try {
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+                LocalDateTime now = LocalDateTime.now();
+                System.out.println(dtf.format(now));
+                List<WebElement> region = driver.findElements(By.xpath("(//div[@class='row row-tall '])"));
+                System.out.println(region.size());
+                for (int z = 0; z < region.size(); z++) {
+                    matches.setLength(0);//
+                    wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//a[@data-stg-id='" + region.get(z).getAttribute("data-stg-id") + "']/div[@class='ply tright name'])")));
+                    List<WebElement> left = driver.findElements(By.xpath("(//a[@data-stg-id='" + region.get(z).getAttribute("data-stg-id") + "']/div[@class='ply tright name'])"));
+                    List<WebElement> right = driver.findElements(By.xpath("(//a[@data-stg-id='" + region.get(z).getAttribute("data-stg-id") + "']/div[@class='ply name'])"));
+                    if (left.isEmpty()) {
+                        left = driver.findElements(By.xpath("(//div[@data-stg-id='" + region.get(z).getAttribute("data-stg-id") + "']/div[@class='ply tright name'])"));
+                        right = driver.findElements(By.xpath("(//div[@data-stg-id='" + region.get(z).getAttribute("data-stg-id") + "']/div[@class='ply name'])"));
+                    }
+                    matches.append(region.get(z).getText()).append("\n");
+                    for (int i = 0; i < left.size(); i++) {
+                        Thread.sleep(500);
+                        System.out.println(left.get(i).getAttribute("innerText"));
+                        matches.append(left.get(i).getText()).append(" vs ").append(right.get(i).getText()).append("\n");
+                    }
+                    System.out.println(matches.toString());
+                    try {
+                        System.out.println(Thread.currentThread());
+                        lock.notify();
+                        lock.wait();
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println();
+                }
+                driver.close();
+                System.out.println("here");
+                driver.close();
+                DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+                LocalDateTime now2 = LocalDateTime.now();
+                System.out.println(dtf2.format(now2));
+            } catch (NoSuchElementException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
---
+++ src/main/java/com/sportbot/SportBot.java
@@ -0,0 +1,51 @@
+package com.sportbot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class SportBot extends TelegramLongPollingBot {
+    private final String BotUsername="adwsportt_bot";
+    private final String BotToken="1270757084:AAEszNCp7-WIBOVO555CDEu4LYxXiTeqJnI";
+    SendMessage sendMessage;
+    Message message;
+    static Object not=new Object();
+    public void onUpdateReceived(Update update) {
+        message=update.getMessage();
+        sendMessage=new SendMessage();
+        if(message.getText().equals("/matches")) {
+            sendMes();
+        }
+    }
+
+    public void sendMes(){
+        MathcesTaker mt = new MathcesTaker();
+        mt.start();
+        synchronized (MathcesTaker.lock) {
+            while (true) {
+                try {
+                    System.out.println(Thread.currentThread());
+                    MathcesTaker.lock.wait();
+                    sendMessage.setChatId(message.getChatId());
+                    sendMessage.setText(MathcesTaker.matches.toString());
+                    execute(sendMessage);
+                    MathcesTaker.lock.notify();
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return BotUsername;
+    }
+
+    public String getBotToken() {
+        return BotToken;
+    }
+}
