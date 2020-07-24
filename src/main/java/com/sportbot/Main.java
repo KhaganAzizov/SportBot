f02f8b469e5ed8d1c2bac248226890931daef34b
Khagan <azizovkhgn@gmail.com>
2020-07-24 12:56:21 +0400

    initial

--- src/main/java/com/sportbot/Main.java
+++ src/main/java/com/sportbot/Main.java
@@ -1,8 +1,22 @@
 package com.sportbot;
 
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ApiContextInitializer.init();
+
+        // Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Register our bot
+        try {
+            botsApi.registerBot(new SportBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
